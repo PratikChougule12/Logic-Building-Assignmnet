42b6b8db171558ff69e3c6c80a6d89eae99a4311
PratikChougule12 <chougulepratik1111@gmail.com>
2025-11-03 23:58:59 +0530

    Add files via upload

---
+++ Assignment_21/Assignment_21_2.java
@@ -0,0 +1,35 @@
+class Logic
+{
+    int iDigit = 0;
+    int iCnt = 0;
+    int  iEvenFrequqncy = 0;
+    int  iOddFrequqncy = 0;
+    void countEvenOddRange(int num)
+    {
+       for(iCnt = 1; iCnt<=iNum;iCnt++)
+       {
+            if(iCnt%2==0)
+            {
+                 iEvenFrequqncy++ ;
+            }
+            else
+            {
+                 iOddFrequqncy++;
+            }
+       }
+       System.out.println(iEvenFrequqncy);
+       System.out.println(iOddFrequqncy);
+    }
+        
+}
+//Time Complexity O(n)
+class Assignment_21_1
+{
+    public static void main(String A[])
+    {
+         Logic lobj = new Logic();
+
+        lobj.countEvenOddRange(50);
+
+    }
+}
