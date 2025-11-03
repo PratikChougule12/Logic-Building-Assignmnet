822faa5b33fdb032881c241000eef82f752f4e55
PratikChougule12 <chougulepratik1111@gmail.com>
2025-11-03 23:50:36 +0530

    Add files via upload

---
+++ Assignment_14/Assignment_14.1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void DisplayDigit(int iNo)
+{
+    if(iNo<0)
+    {
+        iNo = -iNo;
+    }
+    while(1<=iNo)
+    {
+        int iDigit = 0;
+        iDigit = iNo % 10;
+        printf("%d",iDigit);
+        iNo = iNo / 10;
+    }
+}
+//Time Complexity O(n)
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter A number :");
+    scanf("%d",&iValue);
+
+    DisplayDigit(iValue);
+
+    return 0;
+}
