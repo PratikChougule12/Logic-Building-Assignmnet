186b2fb6a8693a85b2243cd03608e808df91ee89
PratikChougule12 <chougulepratik1111@gmail.com>
2025-11-03 23:51:09 +0530

    Add files via upload

---
+++ Assignment_15/Assignment_15.4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int MulDigits(int iNo)
+{
+    int iMul = 0;
+    if(iNo<0)
+    {
+        iNo = -iNo;
+    }
+    while(1<=iNo)
+    {
+        int iDigit = 0;
+        iDigit = iNo % 10;
+        iNo = iNo / 10;
+       iMul = iNo*iNo;
+    }
+    return iMul;
+}
+//Time Complexity O(n)
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter A number :");
+    scanf("%d",&iValue);
+
+   iRet = MulDigits(iValue);
+
+   printf("%d",iRet);
+
+    return 0;
+}
