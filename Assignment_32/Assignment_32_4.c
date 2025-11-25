2ff2ce8e255fb87282db0c16fc11695098f25d6f
PratikChougule12 <chougulepratik1111@gmail.com>
2025-11-26 01:47:53 +0530

    Add files via upload

---
+++ Assignment_32/Assignment_32_4.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    
+    if(iRow != iCol)
+    {
+        printf("Invalid Input \n");
+        printf("Row number and column number should be same\n");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1 ;j <= iCol; j++)
+        {
+            if(i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("*\t");
+            }
+            else if(i == j || i + j == (iCol + 1))
+            {
+                printf("*\t");
+            }
+            else if(i < j && i + j < (iCol + 1))
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("$\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of Rows and Column :\n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
